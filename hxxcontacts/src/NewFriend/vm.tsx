40bac7001f2b84913d4621414e038cbd91de1f2f
wenliang028 <2032797942@qq.com>
2026-02-22 12:26:22 +0800

    Add files via upload

---
+++ hxxcontacts/src/NewFriend/vm.tsx
@@ -0,0 +1,87 @@
+import { WKSDK, Message, CMDContent } from "hxximjssdk";
+import { FriendApplyState, WKApp, ProviderListener } from "@hxx/base";
+import { FriendApply } from "@hxx/base";
+
+export class NewFriendVM extends ProviderListener {
+  friendApplys: FriendApply[] = [];
+  sureLoading: boolean = false;
+  currentFriendApply?: FriendApply;
+
+  async didMount(): Promise<void> {
+    WKApp.shared.friendApplyMarkAllReaded();
+
+    this.friendApplys = await this.getFriendApply();
+    if (this.friendApplys.length === 0) {
+      this.clearFriendApply();
+    }
+    this.notifyListener();
+    // 监听好友申请
+    WKSDK.shared().chatManager.addCMDListener(this.friendRequestCMDListener);
+  }
+
+  didUnMount(): void {
+    // 监听好友申请
+    WKSDK.shared().chatManager.removeCMDListener(this.friendRequestCMDListener);
+  }
+
+  friendSure(apply: FriendApply) {
+    this.sureLoading = true;
+    this.currentFriendApply = apply;
+    this.notifyListener();
+
+    WKApp.dataSource.commonDataSource
+      .friendSure(apply.token || "")
+      .then(() => {
+        apply.status = FriendApplyState.accepted;
+        // WKApp.shared.updateFriendApply(apply);
+        this.delFriendApply(apply);
+        this.sureLoading = false;
+        this.notifyListener();
+      })
+      .catch(() => {
+        this.sureLoading = false;
+        this.notifyListener();
+      });
+  }
+
+  async getFriendApply(): Promise<FriendApply[]> {
+    const fromData = {
+      page_index: 1,
+      page_size: 999,
+    };
+    const res = await WKApp.apiClient.get("/friend/apply", {
+      param: fromData,
+    });
+
+    return res;
+  }
+
+  async delFriendApply(apply: FriendApply): Promise<void> {
+    WKApp.apiClient
+      .delete(`/friend/apply/${apply.to_uid}`)
+      .then(async () => {
+        this.friendApplys = await this.getFriendApply();
+        this.sureLoading = false;
+        this.notifyListener();
+      })
+      .catch(() => {
+        this.sureLoading = false;
+        this.notifyListener();
+      });
+  }
+
+  async clearFriendApply(): Promise<void> {
+    if (WKApp.loginInfo.isLogined()) {
+      WKApp.loginInfo.setStorageItem(`${WKApp.loginInfo.uid}-friend-applys-unread-count`, '0')
+    }
+    await WKApp.apiClient.delete(`/user/reddot/friendApply`);
+  }
+
+  public async friendRequestCMDListener(message: Message) {
+    console.log("收到CMD->", message);
+    const cmdContent = message.content as CMDContent;
+    if (cmdContent.cmd === "friendRequest") {
+      this.friendApplys = await this.getFriendApply();
+    }
+  }
+}
---
+++ hxxcontacts/src/module.tsx
@@ -0,0 +1,147 @@
+import {
+  EndpointCategory,
+  IconListItem,
+  IModule,
+  WKApp,
+  ThemeMode,
+} from "@hxx/base";
+import React from "react";
+import ReactDOM from "react-dom";
+import Blacklist from "./Blacklist";
+import { FriendAdd } from "./FriendAdd";
+import GroupSave from "./GroupSave";
+import { NewFriend } from "./NewFriend";
+import { ContactsListManager } from "./Service/ContactsListManager";
+import { OrganizationalGroupNew } from "./Organizational/GroupNew/index";
+
+export default class ContactsModule implements IModule {
+  id(): string {
+    return "ContactsModule";
+  }
+  init(): void {
+    console.log("【ContactsModule】初始化");
+
+    WKApp.endpointManager.setMethod(
+      "contacts.friendapply.change",
+      () => {
+        ContactsListManager.shared.refreshList();
+      },
+      {
+        category: EndpointCategory.friendApplyDataChange,
+      }
+    );
+
+    // 获取好友未申请添加数量
+    // let unreadCount = 0;
+    // if(WKApp.loginInfo.isLogined()){
+    //   WKApp.apiClient.get(`/user/reddot/friendApply`).then(res=>{
+    //     unreadCount = res.count;
+    //     console.log('====', unreadCount)
+    //     WKApp.mittBus.emit('friend-applys-unread-count', unreadCount)
+    //     WKApp.menus.refresh();
+    //   })
+    // }
+
+    WKApp.endpoints.registerContactsHeader("friends.new", (param: any) => {
+      return (
+        <IconListItem
+          badge={ WKApp.shared.getFriendApplysUnreadCount() }
+          title="新朋友"
+          icon={require("./assets/friend_new.png")}
+          backgroudColor={"var(--wk-color-secondary)"}
+          onClick={() => {
+            WKApp.routeLeft.push(<NewFriend></NewFriend>);
+          }}
+        ></IconListItem>
+      );
+    });
+
+    WKApp.endpoints.registerContactsHeader("groups.save", (param: any) => {
+      return (
+        <IconListItem
+          title="保存的群"
+          icon={require("./assets/icon_group_save.png")}
+          backgroudColor={"var(--wk-color-secondary)"}
+          onClick={() => {
+            WKApp.routeLeft.push(<GroupSave></GroupSave>);
+          }}
+        ></IconListItem>
+      );
+    });
+
+    WKApp.endpoints.registerContactsHeader(
+      "contacts.blacklist",
+      (param: any) => {
+        return (
+          <IconListItem
+            title="黑名单"
+            icon={require("./assets/blacklist.png")}
+            backgroudColor={"var(--wk-color-secondary)"}
+            onClick={() => {
+              WKApp.routeLeft.push(<Blacklist></Blacklist>);
+            }}
+          ></IconListItem>
+        );
+      }
+    );
+
+    WKApp.shared.chatMenusRegister("chatmenus.addfriend", (param) => {
+      const isDark = WKApp.config.themeMode === ThemeMode.dark;
+      return {
+        title: "添加朋友",
+        icon: require(`${
+          isDark
+            ? "./assets/popmenus_friendadd_dark.png"
+            : "./assets/popmenus_friendadd.png"
+        }`),
+        onClick: () => {
+          WKApp.routeLeft.push(
+            <FriendAdd
+              onBack={() => {
+                WKApp.routeLeft.pop();
+              }}
+            ></FriendAdd>
+          );
+        },
+      };
+    });
+    // this.registerOrganizational();
+
+    WKApp.endpoints.registerOrganizationalTool(
+      "contacts.organizational.group.add",
+      (param) => {
+        const channel = param.channel as any;
+        return (
+          <OrganizationalGroupNew channel={channel} render={param.render} />
+        );
+      }
+    );
+
+    WKApp.endpoints.registerOrganizationalLayer(
+      "contacts.organizational.layer",
+      (param) => {
+        const channel = param.channel as any;
+        const div = document.createElement("div");
+        const ref: any = React.createRef();
+        document.body.appendChild(div);
+
+        const remove = () => {
+          if (!ref.current) return;
+          ReactDOM.unmountComponentAtNode(div);
+          document.body.removeChild(div);
+        };
+
+        ReactDOM.render(
+          <OrganizationalGroupNew
+            ref={ref}
+            channel={channel}
+            remove={remove}
+          />,
+          div
+        );
+
+        ref.current.onShowModal();
+      }
+    );
+  }
+}
